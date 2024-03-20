b1b5900d2c96dff020e2120056951f4a377f1f90
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-03-20 12:11:42 +0530

    Extra task

--- familytreedb/create-family.sql
+++ familytreedb/create-family.sql
@@ -3,4 +3,18 @@ CREATE TABLE family
         id        BIGSERIAL PRIMARY KEY,
         parent_id BIGINT REFERENCES family (id),
         name      VARCHAR(255)
+);
+
+CREATE TABLE member
+(
+        id        BIGSERIAL PRIMARY KEY,
+        parent_id BIGINT REFERENCES family (id),
+        name      VARCHAR(255)
+
+)
+
+CREATE TABLE relation (
+        {parent_id,id}        BIGSERIAL PRIMARY KEY,
+        name      VARCHAR(255)
+
 )
